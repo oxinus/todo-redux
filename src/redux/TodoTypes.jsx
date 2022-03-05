9eceb6c3f21b4b8f1660a22a2e3e91d5e012469e
armin jahani <arminjhni@gmail.com>
2022-03-05 20:21:02 +0330

    delete todo types component and define it inside reducer

--- src/redux/TodoTypes.jsx
+++
@@ -1,6 +0,0 @@
-// add our action types to dispatch them to reducer
-
-export const DELETE_TASK = 'delete-task';
-export const ADD_TASK = 'add-task';
-export const EDIT_TASK = 'edit-task';
-export const COMPLETE_TASK = 'complete-task';
