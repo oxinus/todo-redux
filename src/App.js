cbf14bad97136c7e14dbb7a467ba60c7999a5d25
armin jahani <arminjhni@gmail.com>
2022-03-05 22:23:54 +0330

    add provider and set store to it

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import AddTodo from './components/AddTodo';
 import ShowTodo from './components/ShowTodo';
+import { Provider } from 'react-redux';
+import {store} from './redux/store';
 import './App.css'
 
 function App() {
   return (
-    <div className='app'>
-      <AddTodo />
-      <ShowTodo />
-    </div>
+    <Provider store={store}>
+      <div className='app'>
+        <AddTodo />
+        <ShowTodo />
+      </div>
+    </Provider>
   );
 }
 
