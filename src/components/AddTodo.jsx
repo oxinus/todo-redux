51b00e4ca6bf793365269314629076387fa93de9
armin jahani <arminjhni@gmail.com>
2022-03-05 19:46:11 +0330

    add plus button, css style

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -5,6 +5,7 @@ const AddTodo = () => {
     return (
         <div className='addtodo__container'>
             <input />
+            <button><i className='fas fa-plus'></i></button>
         </div>
     )
 }
